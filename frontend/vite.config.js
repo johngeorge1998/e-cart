a6a6c2ac8a27a195f02fe34bce20057b30c55dad
johngeorge1998 <113915115+johngeorge1998@users.noreply.github.com>
2025-02-21 00:25:12 -0800

    buil configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
-});
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",  // Proxy to your local backend during development
+        changeOrigin: true,               // Optional, ensures that the host header is set correctly
+        secure: false,                    // Optional, useful if you're running a local server without SSL
+      },
+    },
+  },
+ });
